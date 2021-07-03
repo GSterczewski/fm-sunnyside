f9209f47dce8fd4db18aa5e2d2f739ac64294861
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-07-03 08:28:07 +0200

    add simple animations to the header,testimonials and section headings

--- animatedText.js
+++ animatedText.js
@@ -1,16 +1,31 @@
 
+const nodes = {
+  mainHeader : document.getElementById('js-animated-text'),
+  header1 : document.getElementById('js-animated-heading-1'), 
+  header2 : document.getElementById('js-animated-heading-2'),
+  testimonials : document.getElementsByClassName('testimonial')
+   
+};
+const headings = {
+  main : 'We are creatives',
+  first: 'Transform your brand',
+  second : "Stand out to the right audience"
+};
 
 class AnimatedText{
-  constructor(targetNode){
+  constructor(targetNode, interval = 50, activeCssClass, staticCssClasses = []){
     this.interval = 0; 
     this.targetNode = targetNode;
     this.currentLetter = 0;
     this.word = [];
+    this.staticCssClasses = staticCssClasses;
+    this.activeCssClass = activeCssClass;
+    this.interval = interval;
   }
   
-  createLetter(letter, cssClasses = []){
+  createLetter(letter){
     const span = document.createElement("span");
-    cssClasses.forEach(cssClass => {
+    this.staticCssClasses.forEach(cssClass => {
       span.classList.add(cssClass);
     })
     if(letter === " "){
@@ -23,8 +38,11 @@ class AnimatedText{
   return span;
   }
 
-  createWord(word){
-    return word.split("").map( letter =>this.createLetter(letter,['animated-text']));
+  createWord(word, splitByWords = false){
+    if(splitByWords){
+      return word.split(" ").map(letter => letter + " ").map( letter =>this.createLetter(letter));
+    }
+    return word.split("").map( letter =>this.createLetter(letter));
   }
 
   appendText(textArray){
@@ -32,27 +50,101 @@ class AnimatedText{
       this.targetNode.appendChild(letter)
     })
   }
-  animate(text){
+  animate(text, splitByWords = false){
     const context = this;
-    this.word = this.createWord(text);
+    this.word = this.createWord(text, splitByWords);
+    
     this.appendText(this.word);
-      this.interval = setInterval(this.run.bind(context),50);
+      this.interval = setInterval(this.run.bind(context),context.interval);
   }
   run(){
     
-    this.word[this.currentLetter].classList.add('animated-text--active');
+    this.word[this.currentLetter].classList.add(this.activeCssClass);
     this.currentLetter = this.currentLetter + 1;
     if( this.currentLetter >= this.word.length ) this.onComplete();
   }
   onComplete(){
     clearInterval(this.interval);
   }
+  
 }
  
 
+function detectScroll(elementId, callback){
+  const scrollBox = document.getElementById(elementId)
+  const isWithin = scrollBox.getBoundingClientRect().bottom - 200 <= window.innerHeight;
+  
+  if(isWithin){
+    callback();
+  }
+};
 
-window.addEventListener('DOMContentLoaded', ()=>{
-  const animatedText = new AnimatedText(document.getElementById('js-animated-text'));
-  animatedText.animate('we are creatives');
+let isMainHeadingDrawn = false;
+let isHeading1Drawn = false;
+let isHeading2Drawn = false;
+let isTestimonialsSlided = false;
+
+function setupAnimations(){
+  if(!isMainHeadingDrawn){
+    const animatedHeader = new AnimatedText(nodes.mainHeader,50,'animated-text--active',['animated-text']);
+    animatedHeader.animate(headings.main);
+    isMainHeadingDrawn = true;
+  }
+  const animatedHeading = new AnimatedText(nodes.header1,20,'fadein-heading--active',['fadein-heading']);
+  const animatedHeading2 = new AnimatedText(nodes.header2,20,'fadein-heading--active',['fadein-heading']);
+
+    const detectScroll1 = () => detectScroll('js-scroll-box-1', ()=>{
+      if(!isHeading1Drawn){
+        animatedHeading.animate(headings.first);
+        isHeading1Drawn = true;
+        window.removeEventListener("scroll",detectScroll1);
+      }
+    });
+
+    const detectScroll2 = () => detectScroll('js-scroll-box-2', ()=>{
+      if(!isHeading2Drawn){
+        animatedHeading2.animate(headings.second);
+        isHeading2Drawn = true;
+        window.removeEventListener("scroll",detectScroll2);
+      }
+    })
+    const slideInTestimonials = () => detectScroll('js-testimonials',()=>{
+      console.log('reached testimonials')
+      if(!isTestimonialsSlided){
+       for(let i = 0; i < nodes.testimonials.length; i++){
+         nodes.testimonials[i].classList.add("swiping-element");
+         nodes.testimonials[i].classList.add(`swiping-element-${i+1}`);
+
+       }
+       
+      }
+      window.removeEventListener("scroll", slideInTestimonials);
+    })
+
+    window.addEventListener("scroll", slideInTestimonials);
+    window.addEventListener("scroll", detectScroll1);
+    window.addEventListener("scroll", detectScroll2);
+}
+
+
+function printFallbackContent(){
+    nodes.mainHeader.innerText = headings.main; 
+    nodes.header1.innerText = headings.first;
+    nodes.header2.innerText = headings.second;
+    isMainHeadingDrawn = true;
+    isHeading1Drawn = true;
+    isHeading2Drawn = true;
+}
+
+function animateOrPrintFallback(){
+  if(window.innerWidth > 500){
+    setupAnimations();
+  } else {
+    printFallbackContent();
+  }
+  
+}
+
+window.addEventListener("resize", animateOrPrintFallback);
+window.addEventListener('DOMContentLoaded', animateOrPrintFallback);
  
-  })
