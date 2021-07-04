22ce0376ca455f6a2bb24da67f0320692189e2de
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-07-04 12:13:59 +0200

    add trasnparent overlay when mobile nav is active, with click listener on it,for closing navigation

--- menu.js
+++ menu.js
@@ -1,15 +1,19 @@
 class MobileNavigation {
-  constructor(elementID){
+  constructor(elementID, overlayID){
     this.element = document.getElementById(elementID);
+    this.overlay = document.getElementById(overlayID);
     this.isVisible = false;
     
   }
   show(){
     this.element.classList.add('header__navigation--visible');
+    this.overlay.classList.add('overlay--visible');
+    this.overlay.addEventListener("click", this.hide.bind(this))
     this.isVisible = true;
   }
   hide(){
     this.element.classList.remove('header__navigation--visible');
+    this.overlay.classList.remove('overlay--visible');
     this.isVisible = false;
   }
   toggle(){
@@ -25,7 +29,7 @@ class MobileNavigation {
 }
 
 window.addEventListener("DOMContentLoaded", ()=>{
-  const navigation = new MobileNavigation('js-mobile-navigation');
+  const navigation = new MobileNavigation('js-mobile-navigation', 'js-overlay');
   const button = document.getElementById("js-mobile-menu-button");
   button.addEventListener("click", navigation.toggle.bind(navigation));
 })
